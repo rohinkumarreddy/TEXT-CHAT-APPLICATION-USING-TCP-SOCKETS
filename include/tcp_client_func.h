0368368929ecd1bf8925e4a4cf775c77b624e6ba
Rohin Kumar Reddy Gubbala <31362218+rohinkumarreddy@users.noreply.github.com>
2017-10-18 01:24:53 -0400

    Add files via upload

---
+++ include/tcp_client_func.h
@@ -0,0 +1,7 @@
+#ifndef tcpclientfun // must be unique name in the project
+#define tcpclientfun
+//tcp_client_func.h
+
+int tcp_client_func(int );
+
+#endif
---
+++ include/tcp_server_func.h
@@ -0,0 +1,7 @@
+#ifndef tcpservfun // must be unique name in the project
+#define tcpserfun
+//tcp_server_func.h
+
+void tcp_server_func(int );
+
+#endif
